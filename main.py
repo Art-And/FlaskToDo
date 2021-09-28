a68a530805e854f18cf0e19e2633c5b695803d88
Andres <hir.islas@outlook.com>
2021-09-28 02:17:15 -0500

    Add first request

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from flask.helpers import make_response
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/hello')
 def hello():
-    return 'Hello world Flask'
+    user_ip = request.remote_addr
+
+    return f'Hello your IP is: {user_ip}'
+
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
