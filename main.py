ed1865fb571666f2c9da1195b2bb228ff8952b43
Andres <hir.islas@outlook.com>
2021-09-28 02:53:43 -0500

    Extends html templates with flask

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/hello')
-def hello():
-    user_ip = request.remote_addr
-
-    return render_template('hello.html', user_ip=user_ip)
+toDos = ['buy coffe', 'Send mail', 'Go to the moon']
 
 
 @app.route('/')
@@ -18,3 +13,14 @@ def index():
     response.set_cookie('user_ip', user_ip)
 
     return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.remote_addr
+    context = {
+        'user_ip': user_ip,
+        'toDos': toDos
+    }
+
+    return render_template('hello.html', **context)
