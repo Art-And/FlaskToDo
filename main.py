9dcbfbd74f153acf09bab43e2d7185457b528065
Andres <hir.islas@outlook.com>
2021-09-28 09:35:29 -0500

    Add new header with bootstrap

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from logging import error
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 toDos = ['buy coffe', 'Send mail', 'Go to the moon']
+
+
+@app.errorhandler(404)
+def not_found(err):
+    return render_template('404.html', error=err, user_ip=request.remote_addr)
 
 
 @app.route('/')
